04bfa7ea91038eec997e888336edd6e0281252e4
Thiago Ladeira <thiago.ladeira@externo-jcatlm.com.br>
2025-09-24 15:31:06 -0300

    Controller

---
+++ GRF_AppCRC.Api.Historico.Api/Controllers/HistoricoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GRF_AppCRC.Api.Historico.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistoricoController : ControllerBase
+    {
+        private readonly CustomerService _service;
+
+        public CustomerController(CustomerService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerDto dto)
+        {
+            await _service.AddCustomerAsync(dto);
+            return Ok("Cliente criado com sucesso!");
+        }
+    }
+}
